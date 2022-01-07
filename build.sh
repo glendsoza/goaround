2bebb45ab7e32fbd1d794adb153eefdcc0e9051d
glendsoza <glendsouza23@gmail.com>
2022-01-07 21:48:37 +0530

    Changed the color of text in form to make it more readable

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ platforms=("darwin/amd64"
 "windows/arm"
 "windows/arm64")
 
-version="0.6"
+version="0.7"
 package_split=(${package//\// })
 package_name=${package_split[-1]}
 for platform in "${platforms[@]}"
